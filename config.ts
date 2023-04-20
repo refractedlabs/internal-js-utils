2636098f3c925324b00c867cd7a51557323d562b
Amin Abbasi <amin@prismprotocol.app>
2023-04-20 11:55:19 +0330

    feat: add api for converting configuration to env variables and saving to .env file

--- config.ts
+++ config.ts
@@ -1,26 +1,57 @@
 import {load} from "js-yaml";
-import {readFile} from "fs/promises";
+import {readFile, writeFile} from "fs/promises";
+
+export async function saveConfigAsEnv(configuration: any, file: string, separateByNewLine: boolean = false) {
+    const m = toEnv(configuration, separateByNewLine)
+    const data = [...m.entries()].map(e => e[0].startsWith("\n") ? "" : `${e[0]}=${e[1]}`).join("\n")
+    await writeFile(file, data, "utf8")
+}
 
 export async function loadConfig<C>(file: string): Promise<C> {
     const config = load(await readFile(file, "utf8")) as C;
-    forceEnv(config)
+    updateConfigurationFromEnv(config)
     return config;
 }
 
-function forceEnv(input: any, prefix?: string): void {
-    for (const key in input) {
+function updateConfigurationFromEnv(configuration: any, prefix?: string): void {
+    for (const key in configuration) {
         const newKey = prefix ? `${prefix}_${key}` : key;
-        if (typeof input[key] === "object" || Array.isArray(input[key])) {
-            forceEnv(input[key], newKey)
+        if (typeof configuration[key] === "object" || Array.isArray(configuration[key])) {
+            updateConfigurationFromEnv(configuration[key], newKey)
         } else {
-            input[key] = parseValue(newKey, input[key])
+            configuration[key] = parseValue(newKey, configuration[key])
         }
     }
 }
 
 function parseValue(key: string, val: any) {
-    const envElement = process.env[key.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`).toUpperCase()];
+    const envElement = process.env[camelCaseToDashCase(key)];
     if (envElement)
         return typeof val === "number" ? Number(envElement) : envElement
     return val
+}
+
+function toEnv(configuration: any, separateByNewLine: boolean = false): Map<string, string> {
+    const m = new Map<string, string>()
+    _toEnv(configuration, m, separateByNewLine)
+    return m
+}
+
+function _toEnv(configuration: any, output: Map<string, string>, separateByNewLine: boolean, prefix?: string) {
+    for (const key in configuration) {
+        const newKey = prefix ? `${prefix}_${key}` : key;
+        if (typeof configuration[key] === "object" || Array.isArray(configuration[key])) {
+            _toEnv(configuration[key], output, separateByNewLine, newKey)
+        } else {
+            output.set(camelCaseToDashCase(newKey), `${configuration[key]}`)
+            if (separateByNewLine && prefix == undefined)
+                output.set(`\n${output.size}`, "")
+        }
+    }
+    if (separateByNewLine && (prefix && !prefix.includes("_"))) //only separates in one level
+        output.set(`\n${output.size}`, "")
+}
+
+function camelCaseToDashCase(input: string): string {
+    return input.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`).toUpperCase()
 }
